b194c35cde93d7b5d56b9dcae36b84658c77dd6b
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-26 18:46:32 +0200

    Add addTask() and hook up to button with input

--- src/Tasks/Tasks.tsx
+++ src/Tasks/Tasks.tsx
@@ -1,17 +1,25 @@
+import { useState } from 'react'
 import './Tasks.css'
 
 export default function Tasks() {
+  const [tasks, setTasks] = useState([] as string[]);
+  const [taskToAdd, setTaskToAdd] = useState("");
+
+  function addTask(task: string) {
+    setTasks([...tasks, task]);
+    setTaskToAdd("");
+  }
 
   return (
     <div className="Tasks gridItem2">
       <h1>Tasks</h1>
       <ul>
-        <li>Task 1</li>
-        <li>Task 1</li>
-        <li>Task 1</li>
+        {tasks.map((task) => (
+          <li>{task}</li>
+        ))}
       </ul>
-      <input type="text" />
-      <button>Add</button>
+      <input type="text" value={taskToAdd} onChange={e => { setTaskToAdd(e.target.value) }} />
+      <button onClick={() => addTask(taskToAdd)}>Add</button>
     </div>
   )
 }
