aa68439c4b3f7039349f941c530b717c6317eeaf
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-26 17:22:20 +0200

    Initial commit

---
+++ src/Tasks/Tasks.tsx
@@ -0,0 +1,19 @@
+import './Tasks.css'
+
+export default function Tasks() {
+
+  return (
+    <div className="Tasks">
+      <h1>Tasks</h1>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 1</li>
+        <li>Task 1</li>
+      </ul>
+      <form>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+    </div>
+  )
+}
