c3cf4f3c4387f517b4e9a259bdc742954fbfbbdb
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-27 13:28:10 +0200

    Extract Timer into separate component

--- src/CurrentTask/CurrentTask.tsx
+++ src/CurrentTask/CurrentTask.tsx
@@ -1,15 +1,13 @@
+import Timer from '../Timer/Timer'
 import './CurrentTask.css'
 
 export default function CurrentTask() {
   return (
     <div className="CurrentTask gridItem1">
       <h1>Current Task</h1>
-      <p>Current task</p>
-      <h3>Time left</h3>
-      <p>20:00</p>
+      <Timer />
       <button>Complete task</button>
       <button>Start new task</button>
-      <button>Reset timer</button>
     </div>
-  )
+  );
 }
---
+++ src/Timer/Timer.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Timer() {
+  return (
+    <div className="Timer">
+      <h1>20:00</h1>
+      <button>Reset timer</button>
+    </div>
+  )
+}
