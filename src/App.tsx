61b7d27bbb135fdcf2af96ac17de444dc23f5986
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-27 15:56:08 +0200

    Make sure a different task is selected when changing tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CurrentTask from './CurrentTask/CurrentTask'
 import Tasks from './Tasks/Tasks'
 
 function App() {
-  const [currentTask, setCurrentTask] = useState<string>("");
+  const [index, setIndex] = useState<number>(0);
   const [tasks, setTasks] = useState<string[]>([]);
 
   function addTask(task: string) {
@@ -12,14 +12,17 @@ function App() {
   }
 
   function startNewTask() {
-    let randomIndex = Math.floor(Math.random() * tasks.length);
-    console.log(randomIndex);
-    setCurrentTask(tasks[randomIndex]);
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * tasks.length);
+    } while (randomIndex === index && tasks.length > 1);
+
+    setIndex(randomIndex);
   }
 
   return (
     <div className="App">
-      {tasks.length > 2 ? <CurrentTask currentTask={currentTask} startNewTask={startNewTask} /> :
+      {tasks.length > 2 ? <CurrentTask currentTask={tasks[index]} startNewTask={startNewTask} /> :
         (
           <div className="App-prompt">
             <p>Add three tasks to get started.</p>
